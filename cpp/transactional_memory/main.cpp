68e08aa5978a623db8509dc162e6a596bfabfeff
Erik Zenker <erik.zenker@citrix.com>
2016-11-04 17:38:45 +0100

    WIP

--- cpp/transactional_memory/main.cpp
+++ cpp/transactional_memory/main.cpp
@@ -6,12 +6,17 @@
 
 //compile: g++ main.cpp -fgnu-tm -lbenchmark
 
+static void inc(int& i) transaction_safe {
+    ++i;
+}
+
+
 static void TransactionalMemoryIncBenchmark(benchmark::State& state) {
-    static int i = 0;
+    static int n = 0;
     while (state.KeepRunning()){
 	for(std::size_t i = 0; i < state.range(0); ++i){
 	    synchronized { 
-		++i;       
+		++n;
 	    }
 	}
 
@@ -19,24 +24,23 @@ static void TransactionalMemoryIncBenchmark(benchmark::State& state) {
 }
 BENCHMARK(TransactionalMemoryIncBenchmark)->Threads(100)->Threads(1000)->Range(1<<10, 1<<20);
 
-
 static void AtomicIncBenchmark(benchmark::State& state) {
-    static std::atomic<int> i(0);
+    static std::atomic<int> n(0);
     while (state.KeepRunning()){
 	for(std::size_t i = 0; i < state.range(0); ++i){
-	    ++i;       
+	    ++n;       
 	}
     }
 }
 BENCHMARK(AtomicIncBenchmark)->Threads(100)->Threads(1000)->Range(1<<10, 1<<20);
 
 static void MutexIncBenchmark(benchmark::State& state) {
-    static int i(0);
+    static int n(0);
     static std::mutex m;
     while (state.KeepRunning()){
 	for(std::size_t i = 0; i < state.range(0); ++i){
 	    std::lock_guard<std::mutex> lock(m);
-	    ++i;
+	    ++n;
 	}
     }
 }
