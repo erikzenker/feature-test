7d3fee17627d0f0c2ee5293bd24cedd74bfbe022
Erik Zenker <erikzenker@citrix.com>
2017-01-27 09:39:20 +0100

    Testing transactional memeory

--- cpp/transactional_memory/main.cpp
+++ cpp/transactional_memory/main.cpp
@@ -7,11 +7,11 @@
 //compile: g++ main.cpp -fgnu-tm -lbenchmark
 
 static void TransactionalMemoryIncBenchmark(benchmark::State& state) {
-    static int i = 0;
+    static int n = 0;
     while (state.KeepRunning()){
 	for(std::size_t i = 0; i < state.range(0); ++i){
 	    synchronized { 
-		++i;       
+		++n;       
 	    }
 	}
 
@@ -21,22 +21,22 @@ BENCHMARK(TransactionalMemoryIncBenchmark)->Threads(100)->Threads(1000)->Range(1
 
 
 static void AtomicIncBenchmark(benchmark::State& state) {
-    static std::atomic<int> i(0);
+    static std::atomic<int> n(0);
     while (state.KeepRunning()){
 	for(std::size_t i = 0; i < state.range(0); ++i){
-	    ++i;       
+	    ++n;       
 	}
     }
 }
 BENCHMARK(AtomicIncBenchmark)->Threads(100)->Threads(1000)->Range(1<<10, 1<<20);
 
 static void MutexIncBenchmark(benchmark::State& state) {
-    static int i(0);
+    static int n(0);
     static std::mutex m;
     while (state.KeepRunning()){
 	for(std::size_t i = 0; i < state.range(0); ++i){
 	    std::lock_guard<std::mutex> lock(m);
-	    ++i;
+	    ++n;
 	}
     }
 }
